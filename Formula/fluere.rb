932dc5b3c32ded76c9c664d4f28307f8ae6053d0
SkuldNorniern <skuldnorniern@gmail.com>
2023-02-14 17:15:18 +0900

    [MODIFY] Bumped version

--- Formula/fluere.rb
+++ Formula/fluere.rb
@@ -4,9 +4,9 @@
 class Fluere < Formula
   desc "Fluere is a cross-platform network tool aimed at pcap sniffing, netflow dump."
   homepage "https://github.com/SkuldNorniern/fluere"
-  url "https://github.com/SkuldNorniern/fluere/releases/download/v.0.3.1/fluere_0.3.1_macos_intel.tar.gz"
-  sha256 "0aa0aeb38981d8beae47968d1f67b93a547f15ef4f0fabe4ded7d495cc4835fd"
-  version "0.3.1"
+  url "https://github.com/SkuldNorniern/fluere/releases/download/v.0.4.0/fluere_0.4.0_macos_intel.tar.gz"
+  sha256 "f7a9a3715310732c6c13f7e9a690f39878106d892178ddad62c75f9c27b6b73d"
+  version "0.4.0"
 
   depends_on arch: :x86_64
   depends_on "libpcap"
