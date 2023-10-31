18824c825a7bc8c0f2753169adb1f9478066cb28
SkuldNorniern <skuldnorniern@gmail.com>
2023-10-31 15:51:55 +0900

    fix: fix sha value mismatch

--- Formula/fluere.rb
+++ Formula/fluere.rb
@@ -3,11 +3,11 @@ class Fluere < Formula
   homepage "https://github.com/SkuldNorniern/fluere"
   on_arm do
     url "https://github.com/SkuldNorniern/fluere/releases/download/v0.6.1/fluere_0.6.1_macos_apple.tar.gz"
-    sha256 "e741102584a0a5db0f6bcafbe858868912278037ef4fe0c6507bbc4e06db4c24"
+    sha256 "20bf318d40f3a128b7f53b6969d702ed6f3ea64eacd1407b7378fbb22d206be0"
   end
   on_intel do
     url "https://github.com/SkuldNorniern/fluere/releases/download/v0.6.1/fluere_0.6.1_macos_intel.tar.gz"
-    sha256 "20bf318d40f3a128b7f53b6969d702ed6f3ea64eacd1407b7378fbb22d206be0"
+    sha256 "e741102584a0a5db0f6bcafbe858868912278037ef4fe0c6507bbc4e06db4c24"
   end
   version "0.6.1"
 
