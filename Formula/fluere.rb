65c7fe23ab506b2537a32bc83291b4bd1af012e0
SkuldNorniern <skuldnorniern@gmail.com>
2023-10-31 15:44:02 +0900

    chore: bump version `0.6.1`

--- Formula/fluere.rb
+++ Formula/fluere.rb
@@ -1,22 +1,25 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Fluere < Formula
   desc "Fluere is a cross-platform network tool aimed at pcap sniffing, netflow dump."
   homepage "https://github.com/SkuldNorniern/fluere"
-  url "https://github.com/SkuldNorniern/fluere/releases/download/v.0.4.0/fluere_0.4.0_macos_intel.tar.gz"
-  sha256 "f7a9a3715310732c6c13f7e9a690f39878106d892178ddad62c75f9c27b6b73d"
-  version "0.4.0"
+  on_arm do
+    url "https://github.com/SkuldNorniern/fluere/releases/download/v.0.6.1/fluere_0.6.1_macos_intel.tar.gz"
+    sha256 "e741102584a0a5db0f6bcafbe858868912278037ef4fe0c6507bbc4e06db4c24"
+  end
+  on_intel do
+    url "https://github.com/SkuldNorniern/fluere/releases/download/v.0.6.1/fluere_0.6.1_macos_apple.tar.gz"
+    sha256 "20bf318d40f3a128b7f53b6969d702ed6f3ea64eacd1407b7378fbb22d206be0"
+  end
+  version "0.6.1"
 
-  depends_on arch: :x86_64
   depends_on "libpcap"
+
 
 
   def install
     bin.install "fluere"
   end
 
-  test do 
+  test do
     system "fluere", "--version"
   end
 end
